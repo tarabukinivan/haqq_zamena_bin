55ee09c460589fe47cf10234db9d8100d0056879
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2023-01-11 09:26:50 +0900

    Create lava043.sh

---
+++ lava043.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+
+cd $HOME
+
+m=0
+while (("${m}" < "22301"))
+do
+    m=$(lavad status |jq .SyncInfo.latest_block_height | xargs)    
+    echo "current height" ${m}
+    sleep 1
+done
+
+cd $HOME 
+git clone https://github.com/lavanet/lava 
+cd lava 
+//git fetch --all 
+//git checkout v0.4.3 
+//make install
+//sudo systemctl restart lavad && sudo journalctl -fu lavad -o cat
