7282d271c56ef76fe8421bfad54862bc4b8fbfc9
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2023-02-08 07:51:20 +0900

    Update lava043.sh

--- lava043.sh
+++ lava043.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 echo "-----------------------------------------------------------------------------"
 
+cd $HOME
+rm -rf lava
+git clone https://github.com/lavanet/lava
+cd $HOME/lava
+git checkout v0.5.2
+
 m=0
 while (("${m}" < "63760"))
 do
     m=$(lavad status |jq .SyncInfo.latest_block_height | xargs)
-    echo "current height" ${m}
+    dig=`echo "digid" | awk '/(^[0-9]{2,6}$)/'${m}`
+    if [ "$dig" = "digid" ];then
+      echo "current height" ${m}
+    else
+      echo "net otveta"
+      m=0
+    fi
     sleep 1
 done
 
-
-cd $HOME/lava
-git fetch --all
-git checkout v0.5.2
+sudo systemctl stop lavad
 make install
 lavad version --long | head
 sudo systemctl restart lavad && sudo journalctl -u lavad -f -o cat
