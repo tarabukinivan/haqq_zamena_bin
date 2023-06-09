5d672b3339e23139d6a254125dea5f05b926a59a
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2023-06-09 23:55:10 +0900

    Create haqq_prov.sh

---
+++ haqq_prov.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+
+binarnik="haqqd"
+nodedir="$HOME/haqq"
+nodeversion="v1.4.0"
+cd $nodedir
+git pull
+git checkout $nodeversion
+make build
+sleep 1
+if test -f ./build/"$binarnik"
+then
+    echo "В build"    
+else
+    echo "В linux"    
+fi 
+for((;;)); do
+    height=$("$binarnik" status |& jq -r ."SyncInfo"."latest_block_height")
+    if ((height == 6134000)); then
+      systemctl stop "$binarnik"
+      
+      if test -f ./build/"$binarnik"
+      then          
+          mv cd $nodedir/build/"$binarnik" $(which "$binarnik")
+      else         
+          mv cd $nodedir/build/linux/"$binarnik" $(which "$binarnik")
+      fi      
+      sudo systemctl restart "$binarnik"
+      echo "restart"
+      break
+    else
+      echo $height
+    fi
+    sleep 1
+done
+journalctl -u "$binarnik" -f -o cat
