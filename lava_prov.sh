305d7c6084b199a0123606f8328c7331e381d1c4
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2023-06-14 01:22:59 +0900

    Create lava_prov.sh

---
+++ lava_prov.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+
+binarnik="lavad"
+nodedir="$HOME/lava"
+nodeversion="v0.13.1"
+cd $nodedir
+git pull
+git checkout $nodeversion
+make build
+sleep 1
+if test -f ./build/"$binarnik"
+then
+    echo "В build"    
+else
+    echo "В linux"    
+fi 
+for((;;)); do
+    height=$("$binarnik" status |& jq -r ."SyncInfo"."latest_block_height")
+    if ((height == 247630)); then
+      systemctl stop "$binarnik"
+      
+      if test -f ./build/"$binarnik"
+      then          
+          mv $nodedir/build/"$binarnik" $(which $binarnik)
+      else         
+          mv $nodedir/build/linux/"$binarnik" $(which $binarnik)
+      fi      
+      sudo systemctl restart "$binarnik"
+      echo "restart"
+      break
+    else
+      echo $height
+    fi
+    sleep 1
+done
+journalctl -u "$binarnik" -f -o cat
