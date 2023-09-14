594edd744449afec7b4dd71f1b1cd404a0fc393b
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2023-09-14 20:09:56 +0900

    Create gitopiaprov.sh

---
+++ gitopiaprov.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+
+binarnik="gitopiad"
+nodedir="$HOME/gitopia"
+nodeversion="v3.0.0"
+cd $nodedir
+git pull
+git checkout $nodeversion
+make build
+sleep 1
+if test -f ./build/"$binarnik"
+then
+    echo "В build"    
+else
+    echo "В bin"    
+fi 
+for((;;)); do
+    height=$("$binarnik" status |& jq -r ."SyncInfo"."latest_block_height")
+    if ((height == 6072000)); then
+      systemctl stop "$binarnik"
+      
+      if test -f ./build/"$binarnik"
+      then          
+          mv $nodedir/build/"$binarnik" $(which $binarnik)
+      else         
+          mv $nodedir/bin/"$binarnik" $(which $binarnik)
+      fi      
+      sudo systemctl restart "$binarnik"
+      echo "restart"
+      break
+    else
+      echo $height
+    fi
+    sleep 1
+done
+journalctl -u "$binarnik" -f -o cat
