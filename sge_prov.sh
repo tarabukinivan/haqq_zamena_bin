6f57e66f6d7913d84e227b0c182abd1cadc77847
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2023-11-18 19:52:08 +0900

    Create sge_prov.sh

---
+++ sge_prov.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+nodedir="$HOME/cascadia"
+binarnik="cascadiad"
+cd $HOME/cascadia
+git pull
+git checkout v0.1.7
+make build
+sleep 1
+if test -f ./build/"$binarnik"
+then
+    echo "В build"    
+else
+    echo "В linux"    
+fi 
+for((;;)); do
+    height=$("$binarnik" status |& jq -r ."SyncInfo"."latest_block_height")
+    if ((height == 2820000)); then
+      systemctl stop "$binarnik"
+      
+      if test -f ./build/"$binarnik"
+      then          
+          mv $nodedir/build/"$binarnik" $(which $binarnik)
+      else         
+          mv $nodedir/build/linux/"$binarnik" $(which $binarnik)
+      fi      
+      sudo systemctl restart "$binarnik"
+      echo "restart"
+      break
+    else
+      echo $height
+    fi
+    sleep 1
+done
+journalctl -u "$binarnik" -f -o cat
