7c1b2ef614e9881829ce0989292835792a0940ec
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2024-01-15 09:51:20 +0900

    Update lava_prov.sh

--- lava_prov.sh
+++ lava_prov.sh
@@ -17,7 +17,7 @@ else
 fi 
 for((;;)); do
     height=$("$binarnik" status |& jq -r ."SyncInfo"."latest_block_height")
-    if ((height == 	764400)); then
+    if ((height == 764400)); then
       systemctl stop "$binarnik"
       
       if test -f ./build/"$binarnik"
