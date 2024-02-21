243a992754c54e3548acdbb62fc99482ae07109a
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2024-02-21 21:37:29 +0900

    Update haqq_prov.sh

--- haqq_prov.sh
+++ haqq_prov.sh
@@ -3,7 +3,7 @@ echo "--------------------------------------------------------------------------
 
 binarnik="haqqd"
 nodedir="$HOME/haqq"
-nodeversion="v1.7.2"
+nodeversion="v1.7.3"
 cd $nodedir
 git pull
 git checkout $nodeversion
@@ -17,7 +17,7 @@ else
 fi 
 for((;;)); do
     height=$("$binarnik" status |& jq -r ."SyncInfo"."latest_block_height")
-    if ((height == 9862000)); then
+    if ((height == 9950000)); then
       systemctl stop "$binarnik"
       
       if test -f ./build/"$binarnik"
