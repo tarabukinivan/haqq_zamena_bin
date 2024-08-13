b517fc3a0fb8fc319bd2826016f5b727df0520f9
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2024-08-13 23:05:16 +0900

    Update warden_prov.sh

--- warden_prov.sh
+++ warden_prov.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 echo "-----------------------------------------------------------------------------"
-
+nodedir="$HOME/wardenprotocol"
 binarnik="wardend"
-
+cd $nodedir
+git pull
+git checkout v0.4.1
+make build
+sleep 1
+if test -f ./build/"$binarnik"
+then
+    echo "В build"    
+else
+    echo "В linux"    
+fi 
 for((;;)); do
-    height=$("$binarnik" status |& jq -r ."sync_info"."latest_block_height")
-    if ((height == 1534500)); then
+    height=$("$binarnik" status |& jq -r ."SyncInfo"."latest_block_height")
+    if ((height == 1675700)); then
       systemctl stop "$binarnik"
-      mv $HOME/wardenprotocol/build/wardend $(which $binarnik)
+      
+      if test -f ./build/"$binarnik"
+      then          
+          mv $nodedir/build/"$binarnik" $(which $binarnik)
+      else         
+          mv $nodedir/build/linux/"$binarnik" $(which $binarnik)
+      fi      
       sudo systemctl restart "$binarnik"
       echo "restart"
       break
