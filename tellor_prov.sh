30a1d485e7c3410040f96b240c50fac4a4ad73ae
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2024-09-11 14:32:40 +0900

    Create tellor_prov.sh

---
+++ tellor_prov.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+
+binarnik="layerd"
+nodedir="$HOME/layer"
+nodeversion="v0.7.1-alpha1"
+cd $nodedir
+git pull
+git checkout $nodeversion
+make build -B
+sleep 1
+if test -f ./build/"$binarnik"
+then
+    echo "В build"    
+else
+    echo "В linux"    
+fi 
+for((;;)); do
+    height=$("$binarnik" status |& jq -r ."sync_info"."latest_block_height")
+    if ((height == 1607234)); then
+      systemctl stop "$binarnik"
+      
+      if test -f ./build/"$binarnik"
+      then          
+          mv $nodedir/build/"$binarnik" $(which $binarnik)
+      else         
+          mv $nodedir/build/linux/"$binarnik" $(which $binarnik)
+      fi      
+      sudo systemctl restart "$binarnik"
+      echo "restart"
+      break
+    else
+      echo $height
+    fi
+    sleep 1
+done
+journalctl -u "$binarnik" -f -o cat
