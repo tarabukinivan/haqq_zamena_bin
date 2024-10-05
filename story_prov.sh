13ca781cf215af8f7eb69034ee6edb7961b917e4
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2024-10-05 11:41:27 +0900

    Update story_prov.sh

--- story_prov.sh
+++ story_prov.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 echo "-----------------------------------------------------------------------------"
 
-binarnik="story"
-nodedir="$HOME/story"
-release="story-linux-amd64-0.9.12-9ae4a63.tar.gz"
-resleasename="story-linux-amd64-0.9.12-unstable-9ae4a63"
-storyservice="story-testnet.service"
-cd $nodedir
-wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/story-public/$release
-tar xvzf $release
-rm $release
-mv $resleasename/"$binarnik" $(which $binarnik)
+cd $HOME
+rm -rf story
+git clone https://github.com/piplabs/story
+cd $HOME/story
+git checkout v0.11.0
+go build -o story ./client
 
 sleep 1
 
 for((;;)); do
     height=$(curl -s http://167.235.39.5:26657/status | jq -r .result.sync_info.latest_block_height)
-    if ((height == 1607234)); then
-      systemctl stop "$binarnik"
-      mv $resleasename/"$binarnik" $(which $binarnik)      
-      sudo systemctl restart "$storyservice"
+    if ((height == 1325860)); then
+      systemctl stop story
+      sudo mv $HOME/story/story $(which story)        
       echo "restart"
       break
     else
@@ -27,4 +22,4 @@ for((;;)); do
     fi
     sleep 1
 done
-journalctl -u "$storyservice" -f -o cat
+sudo systemctl restart story && sudo journalctl -u story -f
