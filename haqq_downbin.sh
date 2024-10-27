0c6fc39310e26ce594322a54c211d158075772e5
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2024-10-27 11:08:49 +0900

    Create haqq_downbin.sh

---
+++ haqq_downbin.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+
+binarnik="haqqd"
+nodedir="$HOME/haqq"
+release="https://github.com/haqq-network/haqq/releases/download/v1.8.2/haqq_1.8.2_linux_amd64.tar.gz"
+cd $HOME
+rm -rf $nodedir
+mkdir -p $nodedir
+wget -O $nodedir/haqq_1.8.2_linux_amd64.tar.gz $release
+sleep 1
+cd $nodedir
+tar -xvzf haqq_1.8.2_linux_amd64.tar.gz
+
+for((;;)); do
+    height=$("$binarnik" status |& jq -r ."SyncInfo"."latest_block_height")
+    if ((height == 13684000)); then
+      systemctl stop "$binarnik" 
+          mv $nodedir/bin/"$binarnik" $(which $binarnik)
+      sudo systemctl restart "$binarnik"
+      echo "restart"
+      break
+    else
+      echo $height
+    fi
+    sleep 1
+done
+journalctl -u "$binarnik" -f -o cat
