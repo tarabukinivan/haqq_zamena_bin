ea113fb334a2b8f7d5566a024a373b8862d06673
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2026-03-30 15:06:11 +0900

    Update setup_server1.sh

--- setup_server1.sh
+++ setup_server1.sh
@@ -42,23 +42,4 @@ apt upgrade -y
 dpkg --configure -a
 apt --fix-broken install -y
 
-# =========================
-# INSTALL CUDA 13.2
-# =========================
-echo "⚡ Installing CUDA 13.2..."
-
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-
-wget https://developer.download.nvidia.com/compute/cuda/13.2.0/local_installers/cuda-repo-ubuntu2204-13-2-local_13.2.0-595.45.04-1_amd64.deb
-dpkg -i cuda-repo-ubuntu2204-13-2-local_13.2.0-595.45.04-1_amd64.deb
-
-cp /var/cuda-repo-ubuntu2204-13-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
-
-apt-get update
-apt-get install -y cuda-toolkit-13-2
-apt-get install -y cuda-drivers
-
-echo "🔄 Rebooting..."
-
-reboot
+sudo reboot
