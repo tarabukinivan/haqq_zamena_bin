1985d080b5b06944e4ffd54e57fbefb4ffe417a5
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2026-03-30 15:12:40 +0900

    Update setup_server2.sh

--- setup_server2.sh
+++ setup_server2.sh
@@ -1,17 +1,33 @@
 # =========================
+# INSTALL CUDA 13.2
+# =========================
+echo "⚡ Installing CUDA 13.2..."
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+
+wget https://developer.download.nvidia.com/compute/cuda/13.2.0/local_installers/cuda-repo-ubuntu2204-13-2-local_13.2.0-595.45.04-1_amd64.deb
+dpkg -i cuda-repo-ubuntu2204-13-2-local_13.2.0-595.45.04-1_amd64.deb
+
+cp /var/cuda-repo-ubuntu2204-13-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
+
+apt-get update
+apt-get install -y cuda-toolkit-13-2
+apt-get install -y cuda-drivers
+
+# =========================
 # NVIDIA CONTAINER TOOLKIT
 # =========================
 echo "🐳 Installing NVIDIA Container Toolkit..."
 
-curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | \
-gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
-sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-apt-get update
-apt-get install -y nvidia-container-toolkit
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
 
 # =========================
 # DOCKER SETUP
