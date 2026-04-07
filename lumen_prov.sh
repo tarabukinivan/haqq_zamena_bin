91c7cc676ccaa3fd57a4d9cb5eb34cbc5958e1c4
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2026-04-07 23:21:35 +0900

    Update lumen_prov.sh

--- lumen_prov.sh
+++ lumen_prov.sh
@@ -3,10 +3,10 @@ binarnik="lumend"
 
 for((;;)); do
     height=$("$binarnik" status | jq -r '.sync_info.latest_block_height')
-    if ((height == 1750000)); then
+    if ((height == 2035000)); then
       systemctl stop "$binarnik"
       systemctl stop lumend2
-      mv ./"$binarnik" $(which $binarnik)
+      mv /root/tmp/"$binarnik" $(which $binarnik)
       sudo systemctl restart "$binarnik"
       sudo systemctl restart lumend2
       echo "restart"
