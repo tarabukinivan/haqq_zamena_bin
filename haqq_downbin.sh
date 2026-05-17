c3652e9ff3bea1561e8c5ad10d6bfebdec855636
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2026-05-17 10:17:23 +0900

    Update haqq_downbin.sh

--- haqq_downbin.sh
+++ haqq_downbin.sh
@@ -7,9 +7,11 @@ cd $nodedir
 
 sleep 1
 chmod +x $nodedir/"$binarnik"
+
+echo "new version: $nodedir/$binarnik"
 for((;;)); do
     height=$("$binarnik" status |& jq -r ."sync_info"."latest_block_height")
-    if ((height == 21135000)); then
+    if ((height == 21551100)); then
       systemctl stop "$binarnik" 
           mv $nodedir/"$binarnik" $(which $binarnik)
       sudo systemctl restart "$binarnik"
